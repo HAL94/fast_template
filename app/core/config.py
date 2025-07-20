e0431f6ddb2e8b2670dad5c992f17a286e386c52
Habib <hqdarwish@gmail.com>
2025-07-20 14:40:24 +0300

    added basic dockerization

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,11 @@
 
+from typing import Union
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AppConfigSettings(BaseSettings):
+    ENV: str = "prod"
+    APP_PORT: int = 8000
 
 
 class PostgresSettings(BaseSettings):
@@ -10,13 +16,13 @@ class PostgresSettings(BaseSettings):
     PG_DB: str
 
 
-class Settings(PostgresSettings):
-    ENV: str = "prod"
+class Settings(PostgresSettings, AppConfigSettings):
     model_config = SettingsConfigDict(
         env_file='.env', env_file_encoding='utf-8')
 
 
 settings = Settings()
+
 
 def get_settings() -> Settings:
     global settings
