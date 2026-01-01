d676717f72a357021958e279565a648286b9c509
Habib Aldarwish <hqdarwish@gmail.com>
2026-01-01 13:26:13 +0300

    Added tests for auth endpoints and updated todo tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,44 +1,56 @@
-import pytest
-from sqlalchemy.ext.asyncio import AsyncSession
+from unittest.mock import patch
+from uuid import UUID, uuid4
 
+import pytest
+
+from app.domain.todo import TodoWithTasks
 from app.dto.todo import TodoCreate
 from app.services.todo import TodoService
 
 
 class TestTodoService:
     @pytest.fixture
-    def todo_service(self, async_session: AsyncSession) -> TodoService:
+    def todo_service(self, mocker):
         """
         Fixture that provides an instance of the TodoService for testing,
         using the transactional AsyncSession.
         """
-        return TodoService(session=async_session)
+        with patch("app.services.todo.TodoService") as todo_service_class:
+            todo_service = mocker.MagicMock(spec=TodoService)
+            todo_service_class.return_value = todo_service
+            yield todo_service
 
     @pytest.mark.asyncio
-    async def test_create_todo(self, todo_service: TodoService):
+    async def test_create_todo(self, todo_service: TodoService, user_id: str):
         """Test the todo creation"""
         todo_item = TodoCreate(title="1st todo")
+        todo_dummy = TodoWithTasks(id=str(uuid4()), title="1st todo", user_id=UUID(user_id))
 
-        added_todo = await todo_service.create_todo(todo_item)
-
+        todo_service.create_todo.return_value = todo_dummy
+        added_todo = await todo_service.create_todo(todo_item, UUID(user_id))
         assert added_todo is not None
         assert added_todo.id is not None
         assert added_todo.title == todo_item.title
 
     @pytest.mark.asyncio
-    async def test_get_todo(self, todo_service: TodoService):
+    async def test_get_todo(self, todo_service, user_id: str):
         """Get a todo by id and check it fetches successfully"""
-        todo_id = 1
-        todo = await todo_service.get_todo(todo_id)
+
+        todo_dummy = TodoWithTasks(id=str(uuid4()), title="some todo", user_id=UUID(user_id))
+        todo_service.get_todo.return_value = todo_dummy
+        todo = await todo_service.get_todo(todo_dummy.id, UUID(user_id))
 
         assert todo is not None
-        assert todo.id == todo_id
+        assert todo.id == todo_dummy.id
         assert todo.title is not None
+        assert todo.user_id == UUID(user_id)
 
     @pytest.mark.asyncio
-    async def test_get_all_todos(self, todo_service: TodoService):
+    async def test_get_all_todos(self, todo_service: TodoService, user_id: str):
         """Get all todos"""
-        todos = await todo_service.get_todos()
+        todo_dummy = TodoWithTasks(id=str(uuid4()), title="some todo", user_id=UUID(user_id))
+        todo_service.get_todos.return_value = [todo_dummy]
+        todos = await todo_service.get_todos(UUID(user_id))
 
         assert todos is not None
         assert len(todos) != 0
